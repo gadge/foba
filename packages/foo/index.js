4dde77777750f8243461e6c874efc4e226f2b673
alkashi <hoyeungw@outlook.com>
2020-02-14 09:35:26 +0800

    Added SimpleVectors to @foba/foo

--- packages/foo/index.js
+++ packages/foo/index.js
@@ -1,8 +1,9 @@
 export { CrosXMatricesRobust, crosXMatricesRobust } from './src/CrosXMatricesRobust'
 export { SimpleMatricesRobust, simpleMatricesRobust } from './src/SimpleMatricesRobust'
-export { SimpleMatrices, simpleMatrices } from './src/SimpleMatrices'
 export { SimpleEntries, simpleEntries } from './src/SimpleEntries'
+export { SimpleMatrices, simpleMatrices } from './src/SimpleMatrices'
 export { SimpleObjects, simpleObjects } from './src/SimpleObjects'
+export { SimpleVectors, simpleVectors } from './src/SimpleVectors'
 export { makeEmbedded } from '@foba/util'
 export { randMatrix } from './util/randMatrix'
 
