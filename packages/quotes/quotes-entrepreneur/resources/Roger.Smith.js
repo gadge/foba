6e7532929c1d75660ed0de9291dd70f95fa5428d
hazen <hoyeungw@outlook.com>
2020-07-22 12:11:40 +0800

    add quotes

---
+++ packages/quotes/quotes-entrepreneur/resources/Roger.Smith.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    figure: 'Roger Smith (executive)',
+    mark: 'Roger B. Smith, chairman, General Motors Corporation, at Albion College, Mich. quoted in: U.\nS. News & World Report Vol92. (1982). p66.',
+    quote: 'The world needs specialists and highly trained people with advanced degrees, no question about it. But the world also needs diversity and versatility. It needs people who know as much about our value system as they do about our solar system system.'
+  },
+  {
+    figure: 'Roger Smith (executive)',
+    mark: 'R.\nB. Smith cited in: Lloyd L. Byars (1987) Strategic management: planning and implementation : concepts and cases p150.',
+    quote: 'We hope this car will be less labor intensive, less material intensive, less everything intensive than anything we have done before.'
+  },
+  {
+    figure: 'Roger Smith (executive)',
+    mark: 'Individuals are trained to recognize recurring elements and common themes.',
+    quote: '[Roger B. Smith] identified the following skills and mental processes required of today’s managers as those acquired and sharpened in the study of the liberal arts.'
+  },
+  {
+    figure: 'Roger Smith (executive)',
+    mark: 'Smith cited in: Micheline Maynard (2007) "Roger B. Smith, 82, Ex-Chief of G.\nM., Dies" in The New York Times 20071201.',
+    quote: 'I don’t expect them to build a big stone monument to me; that’s not my goal in life. I’d like to think that if I did anything extraordinary, it was the work that we did in getting the corporation ready for the 21st century.'
+  },
+  {
+    figure: 'Roger Smith (executive)',
+    mark: 'The liberal arts and the art of management (1987). Cited in: "Bachelor of Arts in Liberal Studies: What is Liberal Studies?" on georgetown.\nedu  about bachelor of arts in liberal studies, 2013.',
+    quote: 'Liberal Arts may ultimately prove to be the most relevant learning model. People trained in the Liberal Arts learn to tolerate ambiguity and to bring order out of apparent confusion. They have the kind of sideways thinking and cross-classifying habit of mind that comes from learning, among other things, the many different ways of looking at literary works, social systems, chemical processes or languages.'
+  },
+  {
+    figure: 'Roger Smith (executive)',
+    mark: 'The liberal arts and the art of management (1987). As cited in: G. Page West, Elizabeth J. Gatewood, Kelly G. Shaver (2009) Handbook of University-wide Entrepreneurship Education. p225.',
+    quote: 'The ultimate impact of the liberal arts on the art of management, then, is a major contribution to the evolution of an ethical and humanistic capitalism -- a system that stimulates innovation, fosters excellence, enriches society, and dignifies work.'
+  },
+  {
+    figure: 'Roger Smith (executive)',
+    mark: 'The liberal arts and the art of management (1987). As cited in: Elizabety A Dreyer (1996) "Excellence in the Profession." Theological Education Vol33.. Nr. 1. (Autumn 1996). p11.',
+    quote: 'Whether you are carving a statue or reorganizing a corporation, you have a vision of what you want to create, as well as a sense of how to make that vision real by bringing different elements together according to an overall pattern... And just as artists communicate their intent through their works, managers must be able to convey their vision in an inspiring and forceful way - in other words to lead - or else that vision will never be fully realized.'
+  },
+]
---
+++ packages/quotes/quotes-financial/resources/Aaron.Brown.js
@@ -0,0 +1,77 @@
+export default [
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch1. The Art of Uncalculated Risk, p2',
+    quote: 'Other people snap up the riskless profits pretty fast and bid the price of calculable risk opportunities to near their fair values. Things get a lot less crowded if you go for the incalculable risks, leaps of faith that cannot be inspected carefully before takeoff. So that is where you find extraordinary opportunities.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch1. The Art of Uncalculated Risk, p12',
+    quote: 'If you want to understand financial markets, and their effects on the economy, you have to understand the trading game. Many short-term price movements are neither random nor caused by economic fundamentals. They\'re caused by investors buying and selling.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch1. The Art of Uncalculated Risk, p23',
+    quote: 'You need a strategy, and a trade or investment decision can be evaluated only in the context of that strategy.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch3. Finance Basics, p59',
+    quote: 'The trading characteristics of a security become more important than its underlying economics. The virtual economics began to drive the physical economy rather than the other way around.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch3. Finance Basics, p67',
+    quote: 'Most people wander through life, carelessly taking whatever risk crosses their paths without compensation, but never consciously accepting extra risk to pick up the money and other good things lying all around them.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch3. Finance Basics, p69',
+    quote: 'There\'s no way to know how good a player you are except by measuring against others.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch4. A Brief History of Risk Denial, p75',
+    quote: 'Monotheism introduced the idea that we should passively accept whatever fate God dealt. Gambling seemed to be a refusal to do that.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch4. A Brief History of Risk Denial, p81',
+    quote: 'If no one gambled against the grain in good times, there would be no winners to inspire people in the bad times.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch4. A Brief History of Risk Denial, p82',
+    quote: 'Fortunes made buying and selling securities have underwritten economic revolutions.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch4. A Brief History of Risk Denial, p83',
+    quote: 'Many financial disasters can be traced to people who thought they were hedging.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch4. A Brief History of Risk Denial, p86',
+    quote: 'From the lottery to raise the funds for the founding of Jamestown, Virginia, in 1607 to the lottery used to pay the interest on Dutch loans to the United States during the Revolutionary War, the development of the United States was funded by gambling.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch5. Pokernomics, p127',
+    quote: 'All exchange stimulates productive activity, whether exchange by gift, gambling, barter, or money transaction.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch6. Son of a Soft Money Bank, p167',
+    quote: 'There is a deep, ancient connection between gambling and divination.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch8. The Games People Play, p231',
+    quote: 'But you can\'t ignore dense people-they\'re not unarmed; they\'re armed differently.'
+  },
+  {
+    figure: 'Aaron Brown (financial author)',
+    mark: 'The Poker Face of Wall Street (2006). Ch9. Who Got Game, p279',
+    quote: 'Mathematical theory, tested in practice and constantly retested, is a valuable aid to play. Mathematics alone will blind you and let others rob you.'
+  },
+]
