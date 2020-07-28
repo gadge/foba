40fe93b941671950e8ab6dd1675c5190891355a0
hazen <hoyeungw@outlook.com>
2020-07-28 23:12:18 +0800

    add flop and flopShuffle to Quotes

--- packages/quotes/quotes-management/index.js
+++ packages/quotes/quotes-management/index.js
@@ -4,4 +4,11 @@ import { Quotes }           from './src/Quotes'
 FlopShuffleMaker.defineFlopShuffle(Quotes)
 FlopShuffleMaker.defineFlop(Quotes)
 
+/**
+ * @property {Function} flop
+ * @property {Function} flopShuffle
+ * @param {string} flop.key
+ * @param {string} flopShuffle.key
+ * @param {number} flopShuffle.size
+ */
 export { Quotes }
